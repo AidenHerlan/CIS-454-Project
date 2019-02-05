4cc89ebcaab42365178a4d69da6d84573edb0bb8
Olivia Flynn <OliviaFlynn@Olivias-MacBook-Pro.local>
2019-02-05 18:16:18 -0500

    FXML project with multiple scenes

--- src/cis/pkg454/project/CIS454Project.java
+++ src/cis/pkg454/project/CIS454Project.java
@@ -6,11 +6,9 @@
 package cis.pkg454.project;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -20,25 +18,14 @@ import javafx.stage.Stage;
 public class CIS454Project extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Press this button and look at output window.");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hey Team, this is really simple but at least we got something!");
-            }
-        });
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Scene scene = new Scene(root);
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hey Guys!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setTitle("Buy or Sell Textbooks Here!!!");
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
