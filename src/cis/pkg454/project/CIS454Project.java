9781b765edcbd44f4938abebc07521cab986db97
AidenHerlan <47157239+AidenHerlan@users.noreply.github.com>
2019-02-12 08:44:26 -0500

    Add files via upload

--- src/cis/pkg454/project/CIS454Project.java
+++ src/cis/pkg454/project/CIS454Project.java
@@ -24,7 +24,7 @@ public class CIS454Project extends Application {
         
         Scene scene = new Scene(root);
         
-        stage.setTitle("Sell or Sell Textbooks Here!!!");
+        stage.setTitle("fhhfhfhj!!!");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
